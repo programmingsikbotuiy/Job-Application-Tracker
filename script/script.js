2bf896fb97fa092ff0dd4e8fdda13d2739289601
programmingsikbotuiy <susmitaswanaker@gmail.com>
2026-03-14 14:26:41 +0600

    btn togole complete

--- script/script.js
+++ script/script.js
@@ -1,11 +1,26 @@
-let currentTab = "All";
-function switchTab(tab){
+let currentTab = "all";
+const tabActive = ["bg-blue-400","border-blue","text-white"];
+const tabInActive = ["bg-transparent","text-slate-700","border-slate-200"];
+function switchTab(tab) {
     console.log(tab);
 
-    const tabs = ["all","interiew","rejected"];
-
-    
+    const tabs = ["all", "interview", "rejected"];
+    for (const t of tabs) {
+        const tabName = document.getElementById("tab-" + t);
+           if(t === tab){
+            tabName.classList.remove(...tabInActive);
+            tabName.classList.add(...tabActive)
+           }
+           else{
+           tabName.classList.remove(...tabActive)
+            tabName.classList.add(...tabInActive)
+           }
+        
+    }
 }
+
+switchTab(currentTab);
+
 
 
 
